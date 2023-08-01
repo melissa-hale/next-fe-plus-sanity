413103dfd56628e1fe515e247729748fbea6f604
Melissa Hale <melissakayh@gmail.com>
2023-08-01 16:04:15 -0500

    fix rows attribute

--- app/(site)/[slug]/ContactForm.tsx
+++ app/(site)/[slug]/ContactForm.tsx
@@ -162,7 +162,7 @@ export default function ContacthtmlForm() {
             <textarea
               className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               id="grid-comments"
-              rows='4'
+              rows={4}
               placeholder="Additional Comments"
             />
             {/* <p className="text-gray-600 text-xs italic">
