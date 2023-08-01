88cc3b6a934d9cef44e4e89df0513ccb2aadf6d9
Melissa Hale <melissakayh@gmail.com>
2023-08-01 16:23:31 -0500

    push logo

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -2,6 +2,9 @@ import { getPages } from '@/sanity/sanity-utils'
 import '../globals.css'
 import type { Metadata } from 'next'
 import Link from 'next/link'
+import Image from 'next/image'
+
+import logo from '../../public/logo.jpeg'
 
 export const metadata: Metadata = {
   title: 'Wall Coverings By Don Dye',
@@ -19,13 +22,17 @@ export default async function RootLayout({
     <html lang="en">
       <body className="max-w-3xl mx-auto py-10">
         <header className="flex items-center justify-between">
-          <div><Link
-            href="/"
-            className="text-lg font-bold font-serif"
-          >
-            Wall Coverings By Don Dye
-          </Link>
-          {/* <p>Serving Austin and Central Texas</p> */}
+          <div>
+            <Link href="/" className="text-lg font-bold font-serif">
+              {/* Wall Coverings By Don Dye */}
+              <Image
+                src={logo}
+                alt='logo'
+                width={250}
+                height={300}
+              />
+            </Link>
+            {/* <p>Serving Austin and Central Texas</p> */}
           </div>
           <div className="flex items-center gap-5 text-sm text-gray-600">
             {pages.map((page) => (
