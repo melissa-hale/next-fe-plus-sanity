d56a88a9697c711dfcdbf8b79871ab0b246c805e
Melissa Hale <melissakayh@gmail.com>
2023-08-01 16:32:37 -0500

    change header

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -6,15 +6,18 @@ export default async function Home() {
   const projects = await getProjects()
 
   return (
-    <div>
+    <div className='`'>
       <h1 className="text-7xl font-extrabold">
         <span className="bg-gradient-to-r from-blue-600 to-sky-900 bg-clip-text text-transparent">
-          Wallpaper Installation, Residential & Commercial
+          Professional Wallpaper Installation
         </span>
         
       </h1>
-      <p className="mt-3 text-xl text-gray-800 mb-5">
+      <p className="mt-3 text-xl text-gray-800 mb-2">
         Fully Insured. Servicing Austin and Central Texas.
+      </p>
+      <p className="mt-3 text-xl text-gray-800 mb-5">
+        Residential & Commercial.
       </p>
       
       
