5a02a31ef2a938a2b022a5660c56f2cf574f07e7
Melissa Hale <melissakayh@gmail.com>
2023-08-10 21:01:46 -0500

    yes styling

--- app/(site)/Components/Sections/Home.tsx
+++ app/(site)/Components/Sections/Home.tsx
@@ -6,12 +6,12 @@ export default async function Home() {
 
   console.log(page)
   return (
-    <div className="p-5 min-h-screen bg-section-bg bg-cover bg-center bg-fixed grid place-items-center h-screen">
+    <div className="p-5 min-h-screen md:min-h-[80vh] lg:min-h-[70vh] bg-section-bg bg-cover bg-center bg-fixed grid place-items-center h-screen">
       <div>
         <h1 className=" bg-amber-100 bg-opacity-70 bg-gradient-to-r from-sky-700 to-blue-500 bg-clip-text text-transparent text-5xl drop-shadow font-extrabold">
           {page.title}
         </h1>
-        <div className="text-lg text-gray-200 mt-7 mb-7 drop-shadow-md">
+        <div className="text-lg text-gray-200 mt-7 mb-7 drop-shadow">
           <PortableText value={page.section_content} />
         </div>
       </div>
