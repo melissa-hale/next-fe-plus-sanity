f8ef6053765adc7710c3edf3b0c777da474d28e9
Melissa Hale <melissakayh@gmail.com>
2023-08-11 15:25:27 -0500

    styles on styles on styles

--- app/(site)/Components/SectionHeader.tsx
+++ app/(site)/Components/SectionHeader.tsx
@@ -12,18 +12,20 @@ export default async function SectionHeader({ slug, children }: Props) {
   return (
     <div
       key={page._id}
-      className="max-w-3xl mx-auto min-h-screen p-8 bg-section-bg "
+      className="max-w-3xl mx-auto min-h-screen bg-section-bg"
     >
       {/* <div key={page._id} className='max-w-3xl mx-auto min-h-screen border-8 border-double border-amber-400 p-8 bg-amber-100 bg-opacity-70'> */}
-      <div className='bg-gray-200 bg-opacity-20 p-3 rounded-2xl shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]'>
-        <h1 className=" text-green-900 text-5xl drop-shadow font-extrabold">
-          {page.title}
-        </h1>
-        <div className="text-lg drop-shadow-md text-gray-800 mt-7 mb-7">
-          <PortableText value={page.content} />
+      <div className='bg-gray-300 bg-opacity-40'>
+        <div className=" p-6">
+          <h1 className=" text-green-900 text-5xl drop-shadow font-extrabold">
+            {page.title}
+          </h1>
+          <div className="text-lg drop-shadow-md text-gray-800 mt-7 mb-7">
+            <PortableText value={page.content} />
+          </div>
         </div>
+        {children}
       </div>
-      {children}
     </div>
   )
 }
