8b0a215d8847d62e949b2d2d0e227cff9aadee3a
Melissa Hale <halem@melissas-mbp.lan>
2023-09-10 21:26:40 -0500

    work

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,7 +2,7 @@ import { getProjects } from '@/sanity/sanity-utils'
 import Gallery from './Components/Gallery'
 import About from './Components/About'
 import Home from './Components/Home'
-import Process from './Components/Process'
+// import Process from './Components/Process'
 
 export default async function Landing() {
   const projects = await getProjects()
